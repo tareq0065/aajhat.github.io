652133dac84060d060d588b55bcca6600146f48a
Andress <andres@mobileappdevelopmentfirm.com>
2018-12-06 17:44:18 +0600

    1st commit

---
+++ precache-manifest.b212f930115970f2c1219ecc4b959e0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "96e15cf95d6e1721e8c48be88ac8164f",
+    "url": "/static/media/aajhat_cricket.96e15cf9.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "64be84ecaf9871999548",
+    "url": "/static/js/main.64be84ec.chunk.js"
+  },
+  {
+    "revision": "082192cdfb2fc95c94c6",
+    "url": "/static/js/1.082192cd.chunk.js"
+  },
+  {
+    "revision": "64be84ecaf9871999548",
+    "url": "/static/css/main.99e0599e.chunk.css"
+  },
+  {
+    "revision": "082192cdfb2fc95c94c6",
+    "url": "/static/css/1.e728223a.chunk.css"
+  },
+  {
+    "revision": "7abddd5249cc7bf0567446b056721032",
+    "url": "/index.html"
+  }
+];
